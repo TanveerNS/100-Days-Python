7cb9c597b5e97827d2f3f3c86e6cf9391b55b516
TanveerNS <44171601+TanveerNS@users.noreply.github.com>
2022-03-17 12:22:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#print("Hello World!")
+#input("What's your Name? ")
+
+# print("Hello " + input("What is your name? "))
+
+#len("Tanveer")
+#print(len("Tanveer"))
+
+print(len (input("What's is your name? ") ))
